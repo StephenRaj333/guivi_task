eae65a175fb4c87110f9f4bf6e3c66a5b1204825
Stephen <stephen@M2P-LAP-831.local>
2024-01-19 17:57:47 +0530

    task updation

--- task_05/script.js
+++ task_05/script.js
@@ -1,6 +1,103 @@
 // TASK DAY _ 05 
 
 // Task 1
+console.log("-----TASK 1 ------");
+
+class Movie {
+    constructor(title, studio, rating = "PG") {
+      this.title = title;
+      this.studio = studio;
+      this.rating = rating;
+    }
+  
+    getPG(movies) {
+      const pgMovies = [];
+  
+      for (let i = 0; i < movies.length; i++) {   
+        if (movies[i].rating === "PG") {
+          pgMovies.push(movies[i]);
+        }   
+      }  
+  
+      return pgMovies;
+    }
+  }
+  
+  const casinoRoyale = new Movie("Casino Royale", "Eon Productions", "PG-13");
+  
+  const moviesArray = [
+    new Movie("Movie1", "Studio1", "PG"),
+    new Movie("Movie2", "Studio2", "R"),
+    new Movie("Movie3", "Studio3", "PG-13"),
+    new Movie("Movie4", "Studio4", "PG"),
+  ];
+  
+  const pgMoviesArray = casinoRoyale.getPG(moviesArray);
+  
+  console.log(casinoRoyale);
+  console.log(pgMoviesArray);
+  
+  
+  console.log("-----TASK 2 ------");
+  // Task 2
+  
+  // Convert the UML diagram to class. - use number for double  
+  
+  class Circle {
+      radius = 1.0;     
+
+      color = "red"; 
+
+      constructor(radius, color) {
+        if (radius !== undefined) {
+          this.radius = radius;
+        }
+    
+        if (color !== undefined) {
+          this.color = color;
+        }
+      }
+    
+      getRadius() {
+        return this.radius;
+      }   
+    
+      setRadius(radius) {
+        this.radius = radius;
+      }
+    
+      getColor() {
+        return this.color;
+      }
+    
+      setColor(color) {   
+        this.color = color;
+      }
+    
+      toString() {
+        return `Circle [radius=${this.radius}, color=${this.color}]`;
+      }
+      
+      getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+      }
+    
+      getCircumference() {
+        return 2 * Math.PI * this.radius; 
+      }
+    }
+    
+    
+    const myCircle = new Circle();
+    console.log(myCircle.toString());
+    
+    const anotherCircle = new Circle(5, "blue");
+    console.log(anotherCircle.toString());
+    console.log("Area:", anotherCircle.getArea());
+    console.log("Circumference:", anotherCircle.getCircumference());    
+
+// Task 3
+console.log("-----TASK 3 ------");
 
 class Person {
     constructor (name,email,age,place) {
@@ -15,7 +112,9 @@ const person = new Person("Stephen","stephen@gmail.com",30,"Tamil Nadu");
 
 console.log(person);
 
-// Task 2 
+
+console.log("-----TASK 4 ------");  
+// Task 4 
 
 class UberFareCalculator {
     constructor (carPreference,distancePerKm,waitingCharges) {
